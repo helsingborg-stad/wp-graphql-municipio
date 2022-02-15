a14cf32a2c2c33cf415c344de3f9d970bcff00f7
nRamstedt <nikolas.ramstedt@gmail.com>
2022-02-15 11:38:25 +0100

    refactor into own method

--- source/php/App.php
+++ source/php/App.php
@@ -6,22 +6,23 @@ class App
 {
     public function __construct()
     {
-        if (!defined('DYNAMIC_CPT_OPTIONS_PREFIX')) {
-            define('DYNAMIC_CPT_OPTIONS_PREFIX', 'options_avabile_dynamic_post_types_');
-            define('DYNAMIC_CPT_OPTIONS_NAME_SUFFIX', '_post_type_name');
+        define('DYNAMIC_CPT_OPTIONS_PREFIX', 'options_avabile_dynamic_post_types_');
+        define('DYNAMIC_CPT_OPTIONS_NAME_SUFFIX', '_post_type_name');
+
+        add_action('register_post_type_args', array($this, 'enableGraphQlForDynamicPostTypes'), 10, 2);
+    }
+
+    public function enableGraphQlForDynamicPostTypes($args, $postType)
+    {
+        $dynamicPostTypes = $this->getDynamicPostTypes();
+        if (\in_array($postType, $dynamicPostTypes)) {
+            $args['show_in_graphql'] = true;
+            $args['graphql_single_name'] = strtolower($args['labels']['singular_name']);
+            $args['graphql_plural_name'] = strtolower($args['labels']['plural_name'] ?? $args['labels']['singular_name'] . 's');
         }
 
-        add_action('register_post_type_args', function ($args, $postType) {
-            $dynamicPostTypes = $this->getDynamicPostTypes();
-            if (\in_array($postType, $dynamicPostTypes)) {
-                $args['show_in_graphql'] = true;
-                $args['graphql_single_name'] = strtolower($args['labels']['singular_name']);
-                $args['graphql_plural_name'] = strtolower($args['labels']['plural_name'] ?? $args['labels']['singular_name'] . 's');
-            }
 
-
-            return $args;
-        }, 10, 2);
+        return $args;
     }
 
     public function getDynamicPostTypes()
