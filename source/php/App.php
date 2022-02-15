018a20b4b306146d7fb624b4b6dac64a9d5703b7
nRamstedt <nikolas.ramstedt@gmail.com>
2022-02-15 13:38:27 +0100

    add support for dynamic taxonomies

--- source/php/App.php
+++ source/php/App.php
@@ -7,9 +7,26 @@ class App
     public const DYNAMIC_CPT_OPTIONS_PREFIX = 'options_avabile_dynamic_post_types_';
     public const DYNAMIC_CPT_OPTIONS_NAME_SUFFIX = '_post_type_name';
 
+    public const DYNAMIC_TAXONOMY_OPTIONS_PREFIX = 'options_avabile_dynamic_taxonomies_';
+    public const DYNAMIC_TAXONOMY_OPTIONS_SLUG_SUFFIX = '_slug';
+
     public function __construct()
     {
         add_action('register_post_type_args', array($this, 'enableGraphQlForDynamicPostTypes'), 10, 2);
+        add_action('register_taxonomy_args', array($this, 'enableGraphQlForDynamicTaxonomies'), 10, 2);
+    }
+
+    public function enableGraphQlForDynamicTaxonomies($args, $taxonomy)
+    {
+        $dynamicTaxonomies = $this->getDynamicTaxonomies();
+        if (\in_array($taxonomy, $dynamicTaxonomies)) {
+            $args['show_in_graphql'] = true;
+            $args['graphql_single_name'] = $taxonomy;
+            $args['graphql_plural_name'] = $taxonomy . 's';
+        }
+
+
+        return $args;
     }
 
     public function enableGraphQlForDynamicPostTypes($args, $postType)
@@ -23,6 +40,25 @@ class App
 
 
         return $args;
+    }
+
+    public function getDynamicTaxonomies()
+    {
+        $dynamicTaxonomy = [];
+
+        $i = 0;
+
+        while ($i !== -1) {
+            $foundTaxonomy = get_option(self::DYNAMIC_TAXONOMY_OPTIONS_PREFIX . $i . self::DYNAMIC_TAXONOMY_OPTIONS_SLUG_SUFFIX, false);
+            if ($foundTaxonomy) {
+                $dynamicTaxonomy[] = $foundTaxonomy;
+                $i++;
+            }
+
+            $i = $foundTaxonomy ? $i : -1;
+        }
+
+        return $dynamicTaxonomy;
     }
 
     public function getDynamicPostTypes()
